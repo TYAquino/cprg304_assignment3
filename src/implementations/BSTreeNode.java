b5e0821e9d6fa8f72703bb23dffeebe9cf93c734
Nabby <trishasalubreaquino@gmail.com>
2024-12-10 22:09:19 -0700

    updateeeee

---
+++ src/implementations/BSTreeNode.java
@@ -0,0 +1,33 @@
+package implementations;
+
+public class BSTreeNode<E extends Comparable<? super E>> {
+	
+		private E data;
+		private BSTreeNode<E> left;
+		private BSTreeNode<E> right;
+		
+		public BSTreeNode(E data) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+		public E getData() {
+			return data;
+		}
+		
+		public BSTreeNode<E> getLeft() {
+			return left;
+		}
+		
+		public BSTreeNode<E> getRight() {
+			return right;
+		}
+		
+		public void setLeft(BSTreeNode<E> left) {
+	        this.left = left;
+	    }
+		
+		public void setRight(BSTreeNode<E> right) {
+			this.right = right;
+		}
+}
